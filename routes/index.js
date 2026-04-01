7a2b396962129b3e55e18659fbc725d18cd4347e
Saja Malek <66196100+sajamalek90@users.noreply.github.com>
2026-04-01 03:30:18 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 // Use like the commented code
 // import { categoryRouter } from './categoryRoutes.js';
 
+import cartRouter      from './cartRoutes.js';
+import couponRouter    from './couponRoutes.js';
+
 const mountRoutes = (app) => {
   // app.use('/api/v1/categories', categoryRouter);
+  
+  app.use('/api/v1/cart',     cartRouter);
+  app.use('/api/v1/coupons',  couponRouter);
 };
 
 export { mountRoutes };
